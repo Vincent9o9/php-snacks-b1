57d71936745379016fc2964272136e23af5db78e
Vincent <23vincent86@gmail.com>
2020-09-16 16:29:00 +0200

    snk1

--- partite.php
+++ partite.php
@@ -5,5 +5,44 @@ Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 -->
 
 <?php
- 
+$partite = [
+    [
+        'squadra1' => 'varese basket',
+        'squadra2' => 'milano basket',
+        'punteggio1' => '85',
+        'punteggio2' => '101'
+    ],
+    [
+        'squadra1' => 'firenze basket',
+        'squadra2' => 'siena basket',
+        'punteggio1' => '70',
+        'punteggio2' => '45'
+    ],
+    [
+        'squadra1' => 'roma basket',
+        'squadra2' => 'asti basket',
+        'punteggio1' => '95',
+        'punteggio2' => '103'
+    ],
+];
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>basket</title>
+    </head>
+    <body>
+        <ul>
+            <?php for($i = 0; $i < count($partite); $i++){ ?>
+            <li>
+                <h1>
+                    <?php echo $partite[$i] ['squadra1'];?> - <?php echo $partite[$i] ['squadra2'];?>
+                    | <?php echo $partite[$i] ['punteggio1'];?> - <?php echo $partite[$i] ['punteggio2'];?>
+                </h1>
+            </li>
+        <?php } ?>
+        </ul>
+    </body>
+</html>
